5fd99ad179f3e9b5cdf7710056d76735078efdf7
ivonneandrea <ivonne.bn@gmail.com>
2024-06-16 19:00:37 -0400

    adding modification

--- Backend/index.js
+++ Backend/index.js
@@ -4,13 +4,13 @@ import cors from 'cors';
 import router from './routes/postRouter.js';
 import {PORT} from "./config.js";
 
-const app = express(); // Creamos la aplicación express
-app.use(cors()); // Habilitamos CORS
-app.use(morgan("dev")); // Habilitamos Morgan para ver las solicitudes en consola
-app.use(express.json()); // Middleware para parsear el cuerpo de las solicitudes POST
-app.use(router); // Habilitamos las rutas
+const app = express(); 
+app.use(cors()); 
+app.use(morgan("dev")); 
+app.use(express.json()); 
+app.use(router); 
 
-// // Configuración del puerto
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
